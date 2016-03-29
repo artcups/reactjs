b6fb37375b7ea56825c32066573096b81cf3eaac
Sebastian Lundh <s.lundh@gmail.com>
2016-03-29 19:56:38 +0200

    simple navigation

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router";
 export default class Layout extends React.Component {
-
+	navigate(){
+		this.props.history.pushState(null, "/");
+	}
 	render() {
 		return (
-			<div></div>
+			<div>
+				<h1>Sebbes sida</h1>
+				{this.props.children}
+				<Link to="archives"><button class="btn">Archived</button></Link>
+				<Link to="settings"><button class="btn">Settings</button></Link>
+				<button onClick={this.navigate.bind(this)}>featured</button>
+			</div>
+
 		);
 	}
 }
