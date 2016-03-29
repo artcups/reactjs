4e137d28d9a6598227599bde3be499da933d41fa
Sebastian Lundh <s.lundh@gmail.com>
2016-03-29 18:39:23 +0200

    State

--- src/js/components/layout.js
+++ src/js/components/layout.js
@@ -1,10 +1,20 @@
 import React from "react";
+
 import  Footer from "./Footer";
 import  Header from "./Header";
+
 export default class Layout extends React.Component {
+	constructor(){
+		super();
+		this.state = {name: "Seb"};
+	}
 	render() {
+		setTimeout(() => {
+			this.setState({name: "Bob"});
+		}, 1000);
 		return (
 			<div>
+				{this.state.name}
 				<Header />
 				<Footer />
 			</div>
